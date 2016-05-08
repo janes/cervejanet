e23609a0bb1a7def2a0961805c99d87f43f18eb6
fjanes <fabio.janes@soaexpert.com.br>
2016-05-08 01:07:29 -0300

    ajustando area de gerenciamento

--- src/main/webapp/app/home/home.controller.js
+++ src/main/webapp/app/home/home.controller.js
@@ -5,29 +5,54 @@
         .module('cervejanetApp')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['$scope', 'Principal', 'LoginService', '$state'];
+    ProdutoController.$inject = ['$scope', '$state', 'Produto', 'ParseLinks', 'AlertService', 'pagingParams', 'paginationConstants'];
 
-    function HomeController ($scope, Principal, LoginService, $state) {
+    function ProdutoController ($scope, $state, Home, ParseLinks, AlertService, pagingParams, paginationConstants) {
         var vm = this;
+        vm.loadAll = loadAll;
+        vm.loadPage = loadPage;
+        vm.predicate = pagingParams.predicate;
+        vm.reverse = pagingParams.ascending;
+        vm.transition = transition;
+        vm.loadAll();
 
-        vm.account = null;
-        vm.isAuthenticated = null;
-        vm.login = LoginService.open;
-        vm.register = register;
-        $scope.$on('authenticationSuccess', function() {
-            getAccount();
-        });
+        function loadAll () {
+        	Produto.query({
+                page: pagingParams.page - 1,
+                size: paginationConstants.itemsPerPage,
+                sort: sort()
+            }, onSuccess, onError);
+            function sort() {
+                var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
+                if (vm.predicate !== 'id') {
+                    result.push('id');
+                }
+                return result;
+            }
+            function onSuccess(data, headers) {
+                vm.links = ParseLinks.parse(headers('link'));
+                vm.totalItems = headers('X-Total-Count');
+                vm.queryCount = vm.totalItems;
+                vm.produtos = data;
+                vm.page = pagingParams.page;
+            }
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
+        }
 
-        getAccount();
+        function loadPage (page) {
+            vm.page = page;
+            vm.transition();
+        }
 
-        function getAccount() {
-            Principal.identity().then(function(account) {
-                vm.account = account;
-                vm.isAuthenticated = Principal.isAuthenticated;
+        function transition () {
+            $state.transitionTo($state.$current, {
+                page: vm.page,
+                sort: vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc'),
+                search: vm.currentSearch
             });
         }
-        function register () {
-            $state.go('register');
-        }
+
     }
 })();
